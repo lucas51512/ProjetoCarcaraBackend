df178e794bab42cbc8dfe38687e6917fceb2d8d3
lucas51512 <lucasbrancalhao454@gmail.com>
2022-10-27 11:00:08 -0300

    Serviço finalizados 2

--- src/atuacao/atuacao.module.ts
+++ src/atuacao/atuacao.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AtuacaoService } from './atuacao.service';
 import { AtuacaoController } from './atuacao.controller';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Module({
   controllers: [AtuacaoController],
-  providers: [AtuacaoService]
+  providers: [AtuacaoService, PrismaService]
 })
 export class AtuacaoModule {}
